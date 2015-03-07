8cfbd67eb38423f4949c5d4c7976640dbbec5d03
cold-lion <coldlionprogramming@gmail.com>
2015-03-07 02:52:59 -0600

    Update [OOP]cold_lion_program.cpp

--- [OOP]cold_lion_program.cpp
+++ [OOP]cold_lion_program.cpp
@@ -19,6 +19,9 @@ private:
 
 int main()
 {
+	//bedroom and kitchen are objects of the class "Room" objects are used to call the functions within the class
+	//Setting an object equal is a more explicit way of saying which constructor you want your object to use
+	//This is for when if a class has more than one constructor
 	Room bedroom = Room();
 	Room kitchen = Room();
 
@@ -26,18 +29,25 @@ int main()
 	bedroom.make_window();
 	bedroom.make_window();
 	bedroom.make_window();
-
+	
+	
+	//get_door() is of type int so we can output it as a regular integer
 	cout << bedroom.get_door() << " " << bedroom.get_window() << endl << endl;
 
 	cout << kitchen.get_door() << endl;
 }
 
+//This is a default constructor, whenever an object is made this is the first program that the computer does
+//You could make another constructor that takes parameters but this one is the default
+//Default constructors do not have parameters
 Room::Room()
 {
+	//Default constructors are usually used to initialize the varibales of the class
 	doors = 0;
 	windows = 0;
 }
 
+//This is where I defined the functions of the class
 void Room::make_door()
 {
 	++doors;
